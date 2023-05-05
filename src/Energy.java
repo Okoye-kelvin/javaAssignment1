ad08da4ace5d652a43de1e5976f128dc3cfa98f4
Okoye-kelvin <121132060+Okoye-kelvin@users.noreply.github.com>
2023-05-05 13:15:00 +0100

    Update Energy.java

--- src/Energy.java
+++ src/Energy.java
@@ -4,5 +4,8 @@ public class Energy extends Physics {
     static double KineticEnergy(double Mass, double Velocity) {
         return 0.5 * Mass * Math.pow(Velocity, 2);
     }
+    static double PotentialEnergy(double Mass, double Gravity, double Height) {
 
+        return Mass * Gravity * Height;
+    }
 }
